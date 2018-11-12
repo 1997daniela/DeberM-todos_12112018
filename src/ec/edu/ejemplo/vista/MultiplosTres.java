79689fb154cd59756aa73c6a70133859f20a6a8d
SuRokaneki <jayrshadowfiend@gmail.com>
2018-11-12 08:39:03 -0500

    Deber de Métodos

---
+++ src/ec/edu/ejemplo/vista/MultiplosTres.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ejemplo.vista;
+
+/**
+ *
+ * @author Flores
+ */
+public class MultiplosTres {
+    public static void main (String args[]) {
+	   for ( int i = 0; i < 101; i+=3 ) {
+	      System.out.println(i);
+	   }	
+    }
+}
+    
+
