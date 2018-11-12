f63f6a9f9c281b08e3575c06585e03db6e73f7cc
SuRokaneki <jayrshadowfiend@gmail.com>
2018-11-12 08:36:18 -0500

    Deber de Métodos

---
+++ src/ec/edu/ejemplo/vista/numerosPares.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ejemplo.vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Flores
+ */
+
+ public class numerosPares {
+ public static void main(String[] args) {
+  //Codigo por www.DesarrollarAplicaciones.com
+ int numero=1;
+ int limite=100;
+ while(numero <= 20){
+  if(numero%2==0){
+   //System.out.println(numero);
+   JOptionPane.showMessageDialog(null, numero); 
+  }
+  numero++;
+  
+ }
+    
+}
+
+ 
+}
+
+
